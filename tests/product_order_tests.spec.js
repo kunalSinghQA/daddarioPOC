beb62badb387b190b8928c96f43f495c88d78b9e
kunalSinghQA <kunal.s@feuji.com>
2023-08-16 20:33:26 +0530

    updating drumbs selection page code

--- tests/product_order_tests.spec.js
+++ tests/product_order_tests.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require("@playwright/test");
 const Sections = require("../fixtures/pageIndex");
 const testData = require("../fixtures/data/homepage.json");
+const productsData = require("../fixtures/data/productCreation.json");
 const { use } = require("../playwright.config");
 require("dotenv").config();
 
@@ -69,16 +70,23 @@ test("Validate get selecting a drumhead option once selected navigate to new pag
     `Verifying ${testData.cokkies.decline} button should be visible`
   ).toBeVisible();
   await homepage.handleCokkies(testData.cokkies.accept);
-  await expect(
-    homepage.selectGetStarted(
-      testData.slickSliderItems[0].drumhead[1].buttonName,
-      testData.slickSliderItems[0].drumhead[0].title
-    ),
-    `Verifying ${testData.slickSliderItems[0].drumhead[1].buttonName} button should be visible`
-  ).toBeVisible();
+  // await expect(
+  //   homepage.selectGetStarted(
+  //     testData.slickSliderItems[0].drumhead[1].buttonName,
+  //     testData.slickSliderItems[0].drumhead[0].title
+  //   ),
+  //   `Verifying ${testData.slickSliderItems[0].drumhead[1].buttonName} button should be visible`
+  // ).toBeVisible();
   await homepage.selectingGetStarted(
     testData.slickSliderItems[0].drumhead[1].buttonName,
     testData.slickSliderItems[0].drumhead[0].title
   );
-  await expect(page).toHaveURL(/drumhead-finder/);
+  const productCreation = new Sections.ProductCreation(page, test);
+  // await expect(page).toHaveURL(/drumhead-finder/);
+  for (let i = 0; i <= productsData.typeOfDrumbs.length; i++) {
+    await expect(
+      productCreation.drumheadOptions(productsData.typeOfDrumbs[i].type),
+      `${productsData.typeOfDrumbs[i].type} is visible`
+    ).toBeVisible();
+  }
 });
