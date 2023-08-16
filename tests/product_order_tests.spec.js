c4fae048829eeaa6b231cee2f65d4a292c5a81a0
Your Name <dinesh.@feuji.com>
2023-08-16 20:35:56 +0530

    adding test

--- tests/product_order_tests.spec.js
+++ tests/product_order_tests.spec.js
@@ -4,7 +4,7 @@ const testData = require("../fixtures/data/homepage.json");
 const { use } = require("../playwright.config");
 require("dotenv").config();
 
-test("Validate products are visible", async ({ page }) => {
+test("'TC-01' Validate products are visible", async ({ page }) => {
   const homepage = new Sections.Homepage(page, test);
   // await homepage.navigateToHomepage(`${use.baseURL}/`);
   await page.goto(`${use.baseURL}/`);
@@ -25,7 +25,7 @@ test("Validate products are visible", async ({ page }) => {
   }
 });
 
-test("Validate get started button is visible and navigation is correct", async ({
+test("'TC-02' Validate get started button is visible and navigation is correct", async ({
   page,
 }) => {
   const homepage = new Sections.Homepage(page, test);
@@ -54,7 +54,7 @@ test("Validate get started button is visible and navigation is correct", async (
   await expect(page).toHaveURL(/drumhead-finder/);
 });
 
-test("Validate get selecting a drumhead option once selected navigate to new page", async ({
+test("'TC-03' Validate get selecting a drumhead option once selected navigate to new page", async ({
   page,
 }) => {
   const homepage = new Sections.Homepage(page, test);
@@ -63,10 +63,6 @@ test("Validate get selecting a drumhead option once selected navigate to new pag
   await expect(
     homepage.acceptOrDeclineCokkies(testData.cokkies.accept),
     `Verifying ${testData.cokkies.accept} button should be visible`
-  ).toBeVisible();
-  await expect(
-    homepage.acceptOrDeclineCokkies(testData.cokkies.decline),
-    `Verifying ${testData.cokkies.decline} button should be visible`
   ).toBeVisible();
   await homepage.handleCokkies(testData.cokkies.accept);
   await expect(
@@ -82,3 +78,19 @@ test("Validate get selecting a drumhead option once selected navigate to new pag
   );
   await expect(page).toHaveURL(/drumhead-finder/);
 });
+
+test("'TC-04' Verify that the selected drum 'Size' is highlighted and NEXT button is enabled", async ({
+  page,
+}) => {
+  const homepage = new Sections.Homepage(page, test);
+  // await homepage.navigateToHomepage(`${use.baseURL}/`);
+  await page.goto(`${use.baseURL}/`);
+  await expect(
+    homepage.acceptOrDeclineCokkies(testData.cokkies.accept),
+    `Verifying ${testData.cokkies.accept} button should be visible`
+  ).toBeVisible();
+  await homepage.handleCokkies(testData.cokkies.accept);
+
+})
+
+
