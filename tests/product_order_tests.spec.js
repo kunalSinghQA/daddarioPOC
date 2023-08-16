f528d35cc4b3c29fa7da8d0048521af9928472b7
kunalSinghQA <kunal.s@feuji.com>
2023-08-16 23:57:13 +0530

    updating genere tests

--- tests/product_order_tests.spec.js
+++ tests/product_order_tests.spec.js
@@ -136,7 +136,9 @@ test("'TC-07' Validate Enviroment values and forward /back button functionality"
     productsData.typeOfDrumbHeads[0].type
   );
   await productCreation.clickingOnNextButton();
-  await productCreation.selectingDrumbSize(productsData.typeOfDrumbSize[0].type);
+  await productCreation.selectingDrumbSize(
+    productsData.typeOfDrumbSize[0].type
+  );
   await productCreation.clickingOnNextButton();
   await productCreation.selectingDrumbReason(
     productsData.typeOfDrumbReason[0].type
@@ -163,5 +165,63 @@ test("'TC-07' Validate Enviroment values and forward /back button functionality"
   await expect(
     productCreation.textInDrumbHead(productsData.textInDrumbGenre),
     `${productsData.textInDrumbGenre} is visible`
+  ).toBeVisible();
+});
+
+test.only("'TC-08' Validate Genre values and forward /back button functionality", async ({
+  page,
+}) => {
+  const homepage = new Sections.Homepage(page, test);
+  // await homepage.navigateToHomepage(`${use.baseURL}/`);
+  await page.goto(`${use.baseURL}/`);
+  await homepage.handleCokkies(testData.cokkies.accept);
+  await expect(
+    homepage.selectGetStarted(
+      testData.slickSliderItems[0].drumhead[1].buttonName,
+      testData.slickSliderItems[0].drumhead[0].title
+    ),
+    `Verifying ${testData.slickSliderItems[0].drumhead[1].buttonName} button should be visible`
+  ).toBeVisible();
+  await homepage.selectingGetStarted(
+    testData.slickSliderItems[0].drumhead[1].buttonName
+  );
+  const productCreation = new Sections.ProductCreation(page, test);
+  await productCreation.selectingDrumType(productsData.typeOfDrumbs[0].type);
+  await productCreation.clickingOnNextButton();
+  await productCreation.selectingDrumhead(
+    productsData.typeOfDrumbHeads[0].type
+  );
+  await productCreation.clickingOnNextButton();
+  await productCreation.selectingDrumbSize(
+    productsData.typeOfDrumbSize[0].type
+  );
+  await productCreation.clickingOnNextButton();
+  await productCreation.selectingDrumbReason(
+    productsData.typeOfDrumbEnvironmnet[0].type
+  );
+  await productCreation.clickingOnNextButton();
+  await productCreation.selectingDrumbEnvrioment(
+    productsData.typeOfDrumbReason[0].type
+  );
+  await productCreation.clickingOnNextButton();
+  for (let i = 0; i < productsData.typeOfDrumbGenre.length; i++) {
+    await expect(
+      productCreation.textInDrumbHead(productsData.typeOfDrumbGenre[i].type),
+      `${productsData.typeOfDrumbGenre[i].type} is visible`
+    ).toBeVisible();
+  }
+  await productCreation.clickingOnBackButton();
+  await expect(
+    productCreation.textInDrumbHead(productsData.textInDrumbEnvironment),
+    `${productsData.textInDrumbEnvironment} is visible`
+  ).toBeVisible();
+  await productCreation.clickingOnNextButton();
+  await productCreation.selectingDrumbGenre(
+    productsData.typeOfDrumbEnvironmnet[0].type
+  );
+  await productCreation.clickingOnNextButton();
+  await expect(
+    productCreation.textInDrumbHead(productsData.textInDrumbSustain),
+    `${productsData.textInDrumbSustain} is visible`
   ).toBeVisible();
 });
